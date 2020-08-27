7405f714ec71513edc72e07a03f4d5aece34465b
JHD Solutions <jhuynh776@gmail.com>
2020-08-27 14:21:29 -0400

    Commit #5 - switched most of the functions from JQuery to Vue. Changed the layout of the form to be columned and not purely rows. Removed generate & back buttons as there is no need for it anymore. Kept reset button which is by the output box now

--- js/script.js
+++ js/script.js
@@ -1,60 +1,9 @@
 /*--------------------------------------------------------------
 >>> TABLE OF CONTENTS:
 ----------------------------------------------------------------
-# Presets
 # Buttons
 	## Submit
-	## Reset
-	## Back
 --------------------------------------------------------------*/
-/*--------------------------------------------------------------
-# Presets
---------------------------------------------------------------*/
-$('#dropdownPresets').change(function(){
-	$('#dropdownPresets option:selected').each(function(){
-		var value = $(this).val();
-
-		if (value == 'toronto'){
-			// 1 mil
-			$('#hash1').val('torontophoto');
-			// 500-900k
-			$('#hash2').val('torontoigers');
-			$('#hash3').val('');
-			// 100-450k
-			$('#hash4').val('exploretoronto');
-			$('#hash5').val('cityshots');
-			$('#hash6').val('ig_streets');
-			$('#hash7').val('igtoronto');
-			$('#hash8').val('imagesoftoronto');
-			$('#hash9').val('torontoinsta');
-			$('#hash10').val('6ixwalks');
-			$('#hash11').val('torontoclx');
-			$('#hash12').val('curiocityto');
-			$('#hash13').val('dailyhiveto');
-			// 50-90k
-			$('#hash14').val('torontopixel');
-			$('#hash15').val('dailyhivetoronto');
-			$('#hash16').val('torontodowntown');
-			$('#hash17').val('tdot_shots');
-			$('#hash18').val('torontostreet');
-			$('#hash19').val('street_ographers');
-			$('#hash20').val('');
-			// 20-40k
-			$('#hash21').val('todotoronto');
-			$('#hash22').val('torontostreetphotography');
-			$('#hash23').val('visualtoronto');
-			$('#hash24').val('');
-			$('#hash25').val('');
-			$('#hash26').val('');
-			$('#hash27').val('');
-			// 10-15k
-			$('#hash28').val('lovetdot');
-			$('#hash29').val('curiositytoronto');
-			$('#hash30').val('');
-		}
-	});
-})
-
 /*--------------------------------------------------------------
 # Buttons
 --------------------------------------------------------------*/
@@ -73,15 +22,15 @@ $('#btnSubmit').click(function(){
 		}
 	});
 
-	alert('total hashtags used: ' + count);
-
 	// Conditional statement
 	// if over 30, doesn't proceed
-	if (count > 30){
-		alert('More than 30 hashtags were used, reduce to 30 or less');
+	if (count > 31){
+		alert('Total hashtags used: ' + count + '\n\r' + 'More than 30 hashtags were used, reduce to 30 or less');
 	}
 	// passing condition
 	else{
+		alert('Total hashtags used: ' + count);
+
 		$('#form1').fadeOut();
 		$('#form2').fadeIn();
 
@@ -105,23 +54,3 @@ $('#btnSubmit').click(function(){
 	}
 
 });
-
-/*--------------------------------------------------------------
-# Reset
---------------------------------------------------------------*/
-$('#btnReset').click(function(){
-	// clears textarea (caption)
-	document.getElementById('caption').value= '';
-	// clears all textboxes
-	$('input[type="textbox"]').each(function(){
-		$(this).val('');
-	})
-});
-
-/*--------------------------------------------------------------
-# Back
---------------------------------------------------------------*/
-$('#btnBack').click(function(){
-	$('#form2').fadeOut();
-	$('#form1').fadeIn();
-})
---
+++ js/vue.js
@@ -0,0 +1,139 @@
+var init = new Vue({
+	el: '#app',
+	data:{
+		caption: '',
+		altTxt: '',
+		tag: '',
+		presets: '',
+		hash1: '',
+		hash2: '',
+		hash3: '',
+		hash4: '',
+		hash5: '',
+		hash6: '',
+		hash7: '',
+		hash8: '',
+		hash9: '',
+		hash10: '',
+		hash11: '',
+		hash12: '',
+		hash13: '',
+		hash14: '',
+		hash15: '',
+		hash16: '',
+		hash17: '',
+		hash18: '',
+		hash19: '',
+		hash20: '',
+		hash21: '',
+		hash22: '',
+		hash23: '',
+		hash24: '',
+		hash25: '',
+		hash26: '',
+		hash27: '',
+		hash28: '',
+		hash29: '',
+		hash30: '',
+		hash31: '',
+		hash32: '',
+		hash33: ''
+	},
+
+	methods:{
+		reset: function(){
+			this.caption = '',
+			this.altTxt = '',
+			this.tag = '',
+			this.hash1 = '',
+			this.hash2 = '',
+			this.hash3 = '',
+			this.hash4 = '',
+			this.hash5 = '',
+			this.hash6 = '',
+			this.hash7 = '',
+			this.hash8 = '',
+			this.hash9 = '',
+			this.hash10 = '',
+			this.hash11 = '',
+			this.hash12 = '',
+			this.hash13 = '',
+			this.hash14 = '',
+			this.hash15 = '',
+			this.hash16 = '',
+			this.hash17 = '',
+			this.hash18 = '',
+			this.hash19 = '',
+			this.hash20 = '',
+			this.hash21 = '',
+			this.hash22 = '',
+			this.hash23 = '',
+			this.hash24 = '',
+			this.hash25 = '',
+			this.hash26 = '',
+			this.hash27 = '',
+			this.hash28 = '',
+			this.hash29 = '',
+			this.hash30 = '',
+			this.hash31 = '',
+			this.hash32 = '',
+			this.hash33 = ''
+		},
+
+		select(a){
+			if (a == 'toronto'){
+        this.tag = '\ntorontopixel\ntorontoclx\ntdot_shots\ngta_features\nlovetdot\nlovetoronto\nthetorontolove\n6ixwalks\ntorontogrvm\ntorontontario\ntorontoyyz\nthankyoutoronto\ntoronto.culture\ntoronto_insta\ncntower\ncity__features\ncuriocitytoronto\nexplore.ontario\ndailyhivetoronto';
+
+        /*=================
+        H A S H T A G S
+        =================*/
+				// 1 mil
+				this.hash1 = 'lovetoronto',
+				// 500-900k
+				this.hash2 = 'torontophotography',
+				this.hash3 = 'torontoigers',
+				// 100-450k
+				this.hash4 = 'dailyhiveto',
+				this.hash5 = 'canada_photolovers',
+				this.hash6 = 'imagesoftoronto',
+				this.hash7 = 'exploreontario',
+				this.hash8 = '6ixgrams',
+				this.hash9 = 'curiocitytoronto',
+				this.hash10 = 'thankyoutoronto',
+				this.hash11 = 'city_features',
+				this.hash12 = 'torontoclx',
+				this.hash13 = '6ixwalks',
+				// 50-90k
+				this.hash14 = 'torontopixel',
+				this.hash15 = 'tdot_shots',
+				this.hash16 = 'torontoculture',
+				this.hash17 = 'torontodowntown',
+				this.hash18 = 'torontophotographers',
+				this.hash19 = 'streetphotograhy',
+				this.hash20 = '',
+				// 20-40k
+				this.hash21 = 'torontophotos',
+				this.hash22 = 'tdotlife',
+				this.hash23 = 'torontomind',
+				this.hash24 = '',
+				this.hash25 = '',
+				this.hash26 = '',
+				this.hash27 = '',
+				// 10-15k
+				this.hash28 = 'gta_features',
+				this.hash29 = 'lovetdot',
+				this.hash30 = 'torontogrvm',
+				// 0-9k
+				this.hash31 = 'thetorontolove',
+				this.hash32 = 'torontocntower',
+				this.hash33 = 'urbatoronto'
+			}
+			else if(a == 'cars'){
+
+			}
+			else if (a == 'ttc'){
+
+			}
+		}
+	}
+});
